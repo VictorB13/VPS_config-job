1950286943afb855981053a356c1c41df6341b83
Victor Butkov <butkovvictor@gmail.com>
2026-06-20 17:48:43 +0300

    iptables.sh fixed for allowing traffic of dns queries

--- scripts/iptables_setup.sh
+++ scripts/iptables_setup.sh
@@ -26,12 +26,12 @@ iptables -P FORWARD DROP
 
 #allow loopback
 echo "Allow LoopBack traffic"
-iptabels -A INPUT -i lo ACCPET
+iptabels -A INPUT -i lo ACCEPT
 iptables -A OUTPUT -j lo ACCEPT
 
 #allow existing connections
 echo "Allow conntrack..."
-iptabhles -A INPUT -m conntrack --ctstate ESTABLISHED,RELETAD -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 #allow ssh connection (via the custom port)
 echo "Allow SSH connections"
@@ -41,12 +41,22 @@ iptables -A INPUT -p tcp --dport "$SSH_PORT" -j ACCEPT
 echo "Allow ICMP (Ping)"
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 
+#Allow outgoing dns queries
+echo "Allow outbound DNS queries"
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+
+#Allow dns anwers to get back to the server
+echo "Allow inbound DNS queries"
+iptables -A INPUT -p udp --sport 53 -j ACCEPT
+iptables -A INPUT -p tcp --sport 53 -j ACCEPT
+
 #allow openvpn connections
 echo "Allow rules for openvpn connections"
 iptables -A INPUT -p udp --dport 1194 -j ACCEPT #opening port 1194 udp
 iptables -A INPUT -i tun+ -j ACCEPT 
 iptables -A FORWARD -i tun+ -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -j MASQUERADE
 
 echo "=== Iptables rules set successfully ==="
 
